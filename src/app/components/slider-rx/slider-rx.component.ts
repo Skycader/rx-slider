8a238e368b98d3057fc4e0000d19007aaf0e0edd
skycader <skycader@yandex.ru>
2024-01-22 01:16:11 +0300

    working fine

--- src/app/components/slider-rx/slider-rx.component.ts
+++ src/app/components/slider-rx/slider-rx.component.ts
@@ -27,11 +27,13 @@ export class SliderRxComponent {
   // This could be any observable
 
   @Input() links: string[] = [];
-  public started = false;
+  public started$ = new BehaviorSubject(false);
   public paused$ = new BehaviorSubject(false);
   public delay$ = this.paused$.pipe(
-    switchMap((isPaused) => timer(isPaused ? 20000 : this.started ? 1000 : 0)),
-    tap((_) => (this.started = true)),
+    switchMap((isPaused) =>
+      timer(isPaused ? 20000 : this.started$.getValue() ? 1000 : 0),
+    ),
+    tap((_) => this.started$.next(true)),
   );
 
   public url$: Observable<string> = interval().pipe(
